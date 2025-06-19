065af898c42293b30c8a5b39a5ad06b40985de9b
Amogh Raut <amalternate2019@gmail.com>
2025-06-19 11:54:59 +0530

    Initial Commit

---
+++ MovieExplorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieExplorer
+//
+//  Created by Amogh Raut   on 19/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
